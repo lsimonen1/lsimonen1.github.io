beb6ba1ad087e916f992d8649ab726130e82e537
lsimonen1 <123672618+lsimonen1@users.noreply.github.com>
2024-10-18 19:11:23 -0500

    changed file paths

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -98,7 +98,7 @@ function Portfolio() {
             <Col md={6} lg={6}>
               <ProjectCard
                 title="Java Script Graphics Town Project"
-                iframeSrc="/portfolio/graphicsTown/for_students/final-grtown.html"
+                iframeSrc="lsimonen1.github.io/public/portfolio/graphicsTown/for_students/final-grtown.html"
                 description="This project for my Computer Graphics class allowed me to dive into 3D modeling and animation using Three.js. I brought a town to life by creating detailed buildings, trees, and various elements, all while incorporating realistic animations to make it feel vibrant and dynamic. It was an incredibly rewarding experience that sharpened my skills in 3D design. If I were to revisit the project, I'd explore more online resources for object creation (I manually created all of the arrays for each face of each object which was very time consuming), which would free up time to experiment with exciting features like shaders. Overall, this project was a fantastic opportunity to grow creatively and technically."
                 width="100%"
                 height="300px"
@@ -116,7 +116,7 @@ function Portfolio() {
             <Col md={6} lg={6}>
               <ProjectCard
                 title="Fake Movie Poster"
-                iframeSrc="/portfolio/Stealinginthewest.html"
+                iframeSrc="./public/portfolio/Stealinginthewest.html"
                 description="This movie poster was crafted during my sophomore year as a showcase of my Photoshop skills. I combined various media from different sources to create a fictional western film. While the composition, layout, and effects are original, I sourced images of actors and money to enhance the design. The inspiration for this poster came from iconic movie posters like 'A Fistful of Dollars' and other modular designs, blending classic elements with my own flair."
                 width="60%"
                 height="450px"
